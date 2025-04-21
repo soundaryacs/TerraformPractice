eb0fd720847870dc4b418fba3aff6ec8147ee339
Soundarya CS <soundaryacs92@gmail.com>
2025-04-21 17:11:06 +0530

    DynamoDB Creation and Locking StateFile

--- Terraform-Day-04-create-s3bucket-backend-statefile/main.tf
+++ Terraform-Day-04-create-s3bucket-backend-statefile/main.tf
@@ -9,3 +9,15 @@ resource "aws_instance" "name" {
 resource "aws_s3_bucket" "name" {
   bucket = var.s3_bucket
 }
+
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+  name = "terraform-state-lock-dynamo"
+  hash_key = "LockID"
+  read_capacity = 20
+  write_capacity = 20
+ 
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
---
+++ Terraform-Day-05-DynamoDB-Statelocking/backend.tf
@@ -0,0 +1,10 @@
+# This backend configuration instructs Terraform to store its state in an S3 bucket.
+ terraform {
+backend "s3" {
+    bucket         = "backendstatefileuploadintos3bucket"  # Name of the S3 bucket where the state will be stored.
+    region         =  "us-east-1"
+    key            = "Terraform-Day-05-DynamoDB-Statelocking/terraform.tfstate" # Path within the bucket where the state will be read/written.
+    dynamodb_table = "terraform-state-lock-dynamo" # DynamoDB table used for state locking, note: first run Day4-s3bucket-DynamoDB-create-for-backend-statefile-remote-locking
+    encrypt        = true  # Ensures the state is encrypted at rest in S3.
+}
+}
