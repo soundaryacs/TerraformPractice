ac7b7fcb2549a8f638582c85aa322fc1abff0b40
Soundarya CS <soundaryacs92@gmail.com>
2025-04-24 15:49:21 +0530

    Terraform module source fetch it from git

--- Terraform-Day-08-terraformmodule/main.tf
+++ Terraform-Day-08-terraformmodule/main.tf
@@ -1,7 +1,7 @@
 module "test" {
-  source = "../Terraform-Day-08-terraformmodule-resource"
-  aws_region = "us-east-1"
-    ami_id = "ami-00a929b66ed6e0de6"
-    aws_instance_type = "t2.micro"
-    instance_name = "test"
+  source              = "git::https://github.com/soundaryacs/TerraformPractice.git//Terraform-Day-08-terraformmodule-resource"
+  aws_region          = "us-east-1"
+  ami_id              = "ami-00a929b66ed6e0de6"
+  aws_instance_type   = "t2.micro"
+  instance_name       = "test"
 }
